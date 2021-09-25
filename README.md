ab9191dd47b4c1dce53f5440ade1d0298a21ba51
Manish Jaiswal <mjaiswal77@yahoo.com>
2021-09-25 20:51:18 +0530

    First release

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # xmlToCsv
-Convert elements from xml file to Csv format
+
+## Introduction 
+This scipt convert elements from xml url to Csv format.
+
+## Assumptions and System requirements ✅
+
+###Requirements
+- Pandas
+
+We assume you already have system with python and pip installed. Use below command to install pandas :-
+
+```
+	pip install pandas
+```
+
+## Run script 💻
+
+```
+python3 xmlToCsv.py
+```
+
+## Parameters
+
+| Params      | Description 		         |
+| ----------- | ---------------------------------|
+| url	      | url containing xml response      |
+| item	      | hierarchy of xml element to iter |
+| filename    | name of the csv file             |
---
+++ xmlToCsv.py
@@ -0,0 +1,27 @@
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+import pandas as pd
+
+
+
+def xmlToCsv(url,item,filename):
+    var_url = urlopen('https://www.europarl.europa.eu/rss/doc/top-stories/en.xml')
+    xmldoc = parse(var_url)
+
+    data = []
+    cols = []
+    for item in xmldoc.iterfind('channel/item'):
+        cols=[subchild.tag for subchild in item.getiterator()][1:]
+        data.append([subchild.text for subchild in item.getiterator()][1:])
+
+    df = pd.DataFrame(data)  # Write in DF and transpose it
+    df.columns = cols  # Update column names
+    df.to_csv(filename,index=False)
+
+
+url = 'https://www.europarl.europa.eu/rss/doc/top-stories/en.xml' # declare url
+item = 'channel/item'  #declare XMl item
+filename = 'channel_Data.csv' # csv filename
+
+#calling function
+xmlToCsv(url,item,filename)
